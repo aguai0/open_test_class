2d7b9d2e5702565c9cf3e9d03daeb7c907a117a6
aguai0 <aguai0@163.com>
2020-08-04 17:26:42 +0800

    modify use h2 db

--- src/main/java/com/example/demo/SecurityConfig.java
+++ src/main/java/com/example/demo/SecurityConfig.java
@@ -1,11 +1,13 @@
 package com.example.demo;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
@@ -14,9 +16,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        //super.configure(http);
-        //配置不需要登陆验证
-        http.authorizeRequests().anyRequest().permitAll().and().logout().permitAll();
+
+        http.authorizeRequests()
+                .antMatchers("/h2-console/**").permitAll();
+        http.csrf().ignoringAntMatchers("/h2-console/**"); // 禁用 H2 控制台的 CSRF 防护
+        http.headers().frameOptions().sameOrigin(); // 允许来自同一来源的H2 控制台的请求
+
+        http.authorizeRequests().anyRequest().permitAll().and().logout().permitAll();//配置不需要登录验证
+
     }
 
 
@@ -24,5 +31,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     public void configure(WebSecurity web) throws Exception {
         web.ignoring().antMatchers("/static/**");
     }
+
 
 }
